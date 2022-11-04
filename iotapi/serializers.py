ec2ec080c35f8b3b42e0701b53a77b677a4e3e17
pablo19-alexander <pablo.pass1905@gmai.com>
2022-11-03 20:38:31 -0500

    modifique el archivo de modelo y hice el serializador de conductor, asignamiento y pasajero

--- iotapi/serializers.py
+++ iotapi/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from iotapi.models import User, IdentificationType, Coordinator, VehicleType, Vehicle
+from iotapi.models import User, IdentificationType, Coordinator, VehicleType, Vehicle, Driver, Assignment, Passenger
 
 
 class IdentificationTypeSerializer(serializers.ModelSerializer):
@@ -42,8 +42,34 @@ class VehicleTypeSerializer(serializers.ModelSerializer):
 
 class VehicleSerializer(serializers.ModelSerializer):
     VehicleType = VehicleTypeSerializer()
-
+    
     class Meta:
         model = Vehicle
         fields = ['id_vehicle', 'vehicle_type',
                   'vehicle_status', 'license_plate']
+
+# Driver
+class DriverSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Driver
+        fields = ['user', 'company_card', 'drivers_license', 'drivers_license_state']
+        
+# assignmanet
+class AssignmentSerializer(serializers.ModelSerializer):
+    
+    user = UserSerializer()
+    vehicle = VehicleSerializer()
+    driver = DriverSerializer()
+    
+    class Meta:
+        model = Assignment
+        fields = ['user', 'vehicle', 'Driver', 'state']
+        
+# passenger
+class PassengerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Passenger
+        fields = ['user', 'passenger_code', 'passenger_permit']
