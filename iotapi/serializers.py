21889f879de32f103408175dd0b523c411d48c63
Manchopacho <84589013+Manchopacho@users.noreply.github.com>
2022-11-11 15:37:51 -0500

    Actualizacion serialaziers de Divide

--- iotapi/serializers.py
+++ iotapi/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from iotapi.models import User, IdentificationType, Coordinator, VehicleType, Vehicle, Driver, Assignment, Passenger
+from iotapi.models import User, IdentificationType, Coordinator, VehicleType, Vehicle, Driver, Assignment, Passenger, Device, DataDevice, DeviceVehicle
 
 
 class IdentificationTypeSerializer(serializers.ModelSerializer):
@@ -25,6 +25,19 @@ class UserSerializer(serializers.ModelSerializer):
         return super(UserSerializer, self).update(instance, validated_data)
 
 
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = ['name', 'description', 'state']
+
+
+class DataDeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataDevice
+        fields = ['device', 'time',
+                  'latitude', 'longitude']
+
+
 class CoordinatorSerializer(serializers.ModelSerializer):
     user = UserSerializer()
 
@@ -41,27 +54,42 @@ class VehicleTypeSerializer(serializers.ModelSerializer):
 
 
 class VehicleSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Vehicle
         fields = ['number_vehicle', 'vehicle_type',
                   'vehicle_status', 'license_plate']
 
+
+class DeviceVehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeviceVehicle
+        fields = ['device', 'vehicle']
+
+
 # Driver
+
+
 class DriverSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+
     class Meta:
         model = Driver
-        fields = ['user', 'company_card', 'drivers_license', 'drivers_license_state']
-        
+        fields = ['user', 'company_card',
+                  'drivers_license', 'drivers_license_state']
+
 # assignmanet
+
+
 class AssignmentSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Assignment
         fields = ['user', 'vehicle', 'Driver', 'state']
-        
+
 # passenger
+
+
 class PassengerSerializer(serializers.ModelSerializer):
     user = UserSerializer()
 
